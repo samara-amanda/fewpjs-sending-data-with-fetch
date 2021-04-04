bf4a1e41eba45abf88bd26d9369560cd077fd485
Sam <samara.amanda01@gmail.com>
2021-04-04 01:18:51 -0400

    commit tree 2f61c00eebf151eb3eae14112c7dc1ab071cb361

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+function submitData(userName, userEmail){
+    let formData = {
+      name: userName,
+      email: userEmail
+    }
+   
+    let configObj = {
+     method: "POST",
+     headers: {
+       "Content-Type": "application/json",
+       "Accept": "application/json"
+     },
+     body: JSON.stringify(formData)
+    }
+   
+     return fetch("http://localhost:3000/users", configObj)
+       .then(function(resp) {
+         return resp.json();
+       })
+       .then(function(obj) {
+         let userId = document.createTextNode(`${obj["id"]}`)
+         document.body.prepend(userId)
+       })
+       .catch(function(error) {
+         let message = document.createTextNode(`${error.message}`)
+         document.body.prepend(message)
+       })
+   }
